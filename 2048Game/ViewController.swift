6aeb52c5e5f2faea96422eb554f6884ff956c3eb
sunnyleeyun <sunnylee945@gmail.com>
2016-09-10 16:52:47 +0800

    Initial Commit

---
+++ 2048Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048Game
+//
+//  Created by Sunny on 2016/9/10.
+//  Copyright © 2016年 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
