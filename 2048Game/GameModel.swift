a4d1e382dc6ddef5301b9a700085e6030aab179f
sunnyleeyun <sunnylee945@gmail.com>
2016-09-12 22:00:54 +0800

    blablabla

--- 2048Game/GameModel.swift
+++ 2048Game/GameModel.swift
@@ -24,7 +24,7 @@ class GameModel: NSObject{
             delegate.scoreChanged(score)
         }
     }
-    var gameBoard: SquareGameBoard<TileObject>
+    var gameboard: SquareGameBoard<TileObject>
     
     let delegate: GameModelProtocol
     
@@ -80,6 +80,95 @@ class GameModel: NSObject{
             self.timer = NSTimer.scheduledTimerWithTimeInterval(queueDelay, target: self, selector: Selector("timerFired: "), userInfo: nil, repeats: false)
         }
     }
+    func insertTile(pos:(Int,Int), value: Int){
+        let(x,y) = pos
+        switch gameboard[x,y]{
+        case .Empty:
+            gameboard[x,y] = TileObject.Tile(value)
+            delegate.insertTile(pos, value: value)
+        }
+        case .Tile:
+        break
+    }
+    func insertTileAtRandonLocation(value: Int){
+        let openSpots = gameboardEmptySpots()
+        if openSpots.count == 0 {
+            return
+        }
+        let idx = Int(arc4random_uniform(UInt43(openSpots.count-1)))
+        let (x,y) =openSpots[idx]
+        insertTile((x, y), value)
+        
+    }
+    func gameboardEmptySpots() -> [(Int, Int)]{
+        var buffer = Array<(Int, Int)>()
+        for i in 0..<dimention{
+            for j in 0..<dimension[i,j]{
+                case .Empty:
+                buffer += [(i,j)]
+                case .Tile:
+                break
+            }
+        }
+        return buffer
+    }
+    func gameboardFull() -> Bool{
+        return gameboardEmptySpots().count == 0
+    }
     
+    func tileBelowHasSameValue(loc: (Int,Int), _ value: Int) -> Bool{
+        let (x,y) = loc
+        if y == dimention-1{
+            return false
+        }
+        switch gameboard[x, y+1]{
+        case let .Tile(v):
+            return v == value
+        default:
+            return false
+        }
+    }
     
+    func tileToRightHasSameValue(loc: (Int, Int), _ value: Int) -> Bool{
+        let (x,y) = loc
+        if x == dimention-1{
+            return false
+        }
+        switch gameboard[x+1,y]{
+        case let .Tile(v):
+            return v == value
+        default:
+            return false
+        }
+    }
+    
+    func userHasLost() -> Bool{
+        if !gameboardFull(){
+            return false
+        }
+        
+        for i in 0..<dimention{
+            for j in 0..<dimention{
+                case .Empty:
+                assert(false, "Gameboard reported itself as full, but we still found an empty tile. This is a logic error.")
+                case let .Tile(v):
+                if self.tileBelowHasSameValue((i, j), v) || self.tileToRightHasSameValue((i,j), v){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    func userHasWon() -> (Bool, (Int, Int)?){
+        for i in 0..<dimention{
+            for j in 0..<dimention{
+                switch gameboard[i,j]{
+                case let .Tile(v) where v >= threshold:
+                    return (true, (i,j))
+                default:
+                    continue
+            }
+        }
+    }
 }
